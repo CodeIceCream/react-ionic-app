d2ea1ea5ad3aa1541e9090c826cfc6355d8ef8a6
daqiang <1484067836@qq.com>
2022-12-13 23:38:02 +0800

    antd-mobile ble 库的引用

---
+++ src/hooks/bluetoothLE.ts
@@ -0,0 +1,29 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { BluetoothLE as ble } from "@awesome-cordova-plugins/bluetooth-le";
+
+export const useBtInit = () => {
+
+  const [scanDevice, setScanDevice] = useState()
+  const onClick = useCallback(async () => {
+    // permission
+    const { hasPermission } = await ble.hasPermission()
+    console.log(`Test hasPermission`, JSON.stringify(hasPermission));
+    if (!hasPermission) {
+      const { requestPermission } = await ble.requestPermission()
+      console.log(`Test requestPermission`, JSON.stringify(requestPermission));
+    }
+    const { isLocationEnabled } = await ble.isLocationEnabled()
+    console.log(`Test isLocationEnabled`, JSON.stringify(isLocationEnabled));
+    if (!isLocationEnabled) {
+      const { requestLocation } = await ble.requestLocation()
+      console.log(`Test requestLocation`, JSON.stringify(requestLocation));
+    }
+
+  }, [])
+
+  useEffect(() => {
+
+  }, [])
+  return { onClick, scanDevice }
+}
+

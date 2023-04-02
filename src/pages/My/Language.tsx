36926f6d743077929d066ef898d5b1291455fe06
daqiang <1484067836@qq.com>
2023-04-02 22:06:42 +0800

    Component: add Language

---
+++ src/pages/My/Language.tsx
@@ -0,0 +1,59 @@
+import { IonIcon } from '@ionic/react';
+import { List, Picker } from 'antd-mobile';
+import { PickerColumnItem, PickerValue } from 'antd-mobile/es/components/picker-view';
+import { earthOutline } from 'ionicons/icons';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const lngs: PickerColumnItem[] = [
+  { label: 'English', value: 'en-US' },
+  { label: '中文', value: 'zh-CN' }
+]
+
+function Language() {
+  const { t, i18n } = useTranslation()
+  const [visible, setVisible] = useState(false)
+  const [lang, setLang] = useState<PickerValue[]>()
+
+  useEffect(() => {
+
+  }, [i18n.language])
+
+  const onLanguageConfirm = (lang: PickerValue[]) => {
+    setLang(lang)
+    console.log(lang);
+    console.log(i18n.language);
+
+    i18n.changeLanguage(lang[0]!)
+  }
+
+  const getLanguageLabel = (lang: string) => {
+    const targetLang = lngs.find(langObj => langObj.value === lang)
+    return targetLang?.label
+  }
+
+
+  return (
+    <>
+      <List.Item
+        prefix={
+          <IonIcon icon={earthOutline} />
+        }
+        extra={getLanguageLabel(i18n.language)}
+        onClick={() => { setVisible(true) }}>
+        {t('my.language')}
+      </List.Item>
+      <Picker
+        columns={[lngs]}
+        visible={visible}
+        onClose={() => {
+          setVisible(false)
+        }}
+        value={lang}
+        onConfirm={onLanguageConfirm}
+      />
+    </>
+  )
+}
+
+export default Language
